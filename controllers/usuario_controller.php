d48d175c9cbfdb36f673d973fc92b310029b51b6
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-09-19 12:45:20 -0300

    se modifico urls.php y carpeta controllers

---
+++ controllers/usuario_controller.php
@@ -0,0 +1,29 @@
+<?php 
+
+/**
+* 
+*/
+class Usuario_Controller
+{
+	//aca habria un include del model paciente
+
+	function __construct(argument)
+	{
+		# code...
+	}
+	static function create(){
+		//aca se llamaria a la view que corresponda
+	}
+	static function read(){
+		//aca se llamaria a la view que corresponda	
+	}
+	static function update(){
+		//aca se llamaria a la view que corresponda
+	}
+	static function delete(){
+		//aca se llamaria a la view que corresponda
+	}
+}
+
+
+?>
