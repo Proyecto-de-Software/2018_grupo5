fef7a04cdb487dbb3b3383f3abeb2664efa7384c
Cristian Steib <cristian.steib@avature.net>
2018-09-19 13:11:19 -0300

    remove my name in files

--- core/url_dispatcher/Dispatcher.php
+++ core/url_dispatcher/Dispatcher.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cristian
- * Date: 04/09/18
- * Time: 20:40
- */
 
 class Dispatcher {
     private $urls;
--- core/url_dispatcher/Matcher.php
+++ core/url_dispatcher/Matcher.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cristian
- * Date: 07/09/18
- * Time: 00:29
- */
 
 abstract class Matcher {
 
