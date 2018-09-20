37a819941de74c72c919a878af1740fce29c3159
Cristian Steib <steibkhriz@gmail.com>
2018-09-20 19:41:07 -0300

    Tomando forma el matcher path

--- src/core/url_dispatcher/MatcherPath.php
+++ src/core/url_dispatcher/MatcherPath.php
@@ -1,13 +1,24 @@
 <?php
 
+/**@example <id:integer>
+ * @example <id:integer:2>   two integers
+ * @example <id:integer:2,5>   min two times , max five times for integers valid = 22 , 333 ,1234 ,11111
+ * @example <id:integer:1..3> integers in rage 1..3
+ * @example <something:slug>
+ * @example <name:string>
+ * @example <name:string:>
+ */
+
 class MatcherPath extends Matcher {
 
-     const re_integer = "([0..9]+)";
-     const re_string = "([a-zA-Z]+)";
 
+    const capture_group_template = "(?P<%id%>%regex%)";
+
+    const re_integer = "(^[0-9]+)";
+    const re_string = "(^[a-zA-Z]+)";
 
     function getParameters($url_request) {
-        // TODO: Implement getParameters() method.
+
     }
 
     function isThis($url_request) {
