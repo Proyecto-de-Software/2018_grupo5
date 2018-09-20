0b94f722188a5d1adec82c621e6651ba1ee74fdc
Cristian Steib <cristian.steib@avature.net>
2018-09-20 12:56:41 -0300

    test htaccess for static files

--- src/core/url_dispatcher/Matcher.php
+++ src/core/url_dispatcher/Matcher.php
@@ -4,7 +4,7 @@ abstract class Matcher {
     public $url_pattern;
 
     function __construct($url_pattern) {
-        $this->url_pattern = $url_pattern;
+        $this->url_pattern = '/' . $url_pattern . '/';
     }
 
     abstract function isThis($url_request);
