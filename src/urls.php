784a04c5fdbb1b21794aaad4bda39dc46971642d
Cristian Steib <cristian.steib@avature.net>
2018-09-20 10:56:02 -0300

    fix urls

--- src/urls.php
+++ src/urls.php
@@ -2,36 +2,14 @@
 /**
  * Rutas disponibles para la aplicacion
  */
-include_once('core/url_dispatcher/Path.php');
-$entidad='sdfa';
-$funcion='sdfafsda';
+include_once(CODE_ROOT . '/core/url_dispatcher/Path.php');
+
 $urls = [
 
-    Path::path_re("^/api/pacientes/^([a-zA-Z]+)/$", 'Pacientes_view::ver_pacientes'),
+    Path::path_re("^/api/pacientes/^([a-zA-Z]+)/$", 'PacientesController::ver_pacientes'),
 
-    Path::path("/api/personas/<id:integer>/<dni:as>", 'aView::aMethod') ,
+    Path::path("/api/personas/<id:integer>/<dni:as>", 'UsuarioController::aMethod'),
 
-    Path::path("/usuario/ver/(fecha)/(sueldo)",'aView::aMethod') ,
-
-    //************************  Urls módulo usuario ********************************//
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-
-	//************************  Urls módulo paciente ********************************//
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion) ,
-
-    Path::path("/".$entidad."/".$funcion."/", $entidad."_controller"."::".$funcion)
+    Path::path("/usuario/ver/(fecha)/(sueldo)", 'RolesController::aMethod'),
 
 ];
