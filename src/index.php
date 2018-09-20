a03d17935cc5914d31ffc0c17028471279ba9c1e
Cristian Steib <steibkhriz@gmail.com>
2018-09-19 23:00:05 -0300

    testing htaccess on server

--- src/index.php
+++ src/index.php
@@ -3,13 +3,10 @@ include_once ("urls.php");
 include_once('core/url_dispatcher/Dispatcher.php');
 
 $url_request = $_SERVER['REQUEST_URI']; //esta devolviendo algo como ex: grupo5/usuarios/alta
-echo $url_request;
+echo $url_request . " ";
 
 $dispatcher = new Dispatcher($urls, 'views/');
 
 echo $dispatcher->run($url_request);
 
 echo $url_request;
-
-
-?>
