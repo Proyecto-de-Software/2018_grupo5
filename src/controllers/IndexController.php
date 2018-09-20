91b597da9c0209909e8bad4a91e56d2fc9a042b5
Cristian Steib <cristian.steib@avature.net>
2018-09-20 13:14:48 -0300

    req once path

---
+++ src/controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cristian
+ * Date: 20/09/18
+ * Time: 12:53
+ */
+
+namespace controllers;
+
+
+class IndexController {
+
+    public static function render(){
+        echo "Hola mundo";
+    }
+
+}
