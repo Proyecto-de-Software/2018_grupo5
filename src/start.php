02a265f435a56d39f344a9b4ff55db19cd325c21
Cristian Steib <cristiansteib@gmail.com>
2018-09-19 22:00:16 -0300

    re-organizacion de las carpetas

--- src/start.php
+++ src/start.php
@@ -3,6 +3,7 @@ include_once ("urls.php");
 include_once('core/url_dispatcher/Dispatcher.php');
 
 $url_request = $_SERVER['REQUEST_URI']; //esta devolviendo algo como ex: grupo5/usuarios/alta
+echo $url_request;
 
 $dispatcher = new Dispatcher($urls, 'views/');
 
