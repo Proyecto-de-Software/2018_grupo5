d527f920367daa80953f45c8e97ef2a100cfd644
Cristian Steib <cristian.steib@avature.net>
2018-09-20 13:08:08 -0300

    urls

--- src/core/url_dispatcher/MatcherRegex.php
+++ src/core/url_dispatcher/MatcherRegex.php
@@ -14,6 +14,9 @@ class MatcherRegex extends Matcher{
 
     function isThis($url_request) {
         $ok = preg_match($this->url_pattern, $url_request, $matches, PREG_OFFSET_CAPTURE);
+        if ($ok){
+            echo "match with :" . $this->url_pattern . " /// ";
+        }
         return $ok;
     }
 
