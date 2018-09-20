83297e7f8315bef34fd9f0b43eac5d9d21aa7e47
Cristian Steib <cristian.steib@avature.net>
2018-09-20 17:09:57 -0300

    getParameters regex path

--- src/urls.php
+++ src/urls.php
@@ -8,7 +8,7 @@ $urls = [
     Path::path_re("/", 'IndexController::render'),
     Path::path_re("/index.html", 'IndexController::render'),
     Path::path_re("/home/urlLinda/", 'IndexController::render'),
-    Path::path_re("/usuario/([a-zA-Z]+)/", 'IndexController::render'),
+    Path::path_re("/usuario/([a-zA-Z]+)/([a-zA-Z]+)/", 'IndexController::render'),
 
     Path::path_re("/api/pacientes/([a-zA-Z]+)/", 'PacientesController::ver_pacientes'),
 
