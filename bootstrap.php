277288b8b9261e1f4e9da2730cdf2f2a96af95b4
Sebastián Muiña <sebastiangabrielm@gmail.com>
2018-09-21 14:19:06 -0300

    reacomodamiento de carpetas

--- bootstrap.php
+++ bootstrap.php
@@ -8,9 +8,9 @@ require_once (__DIR__. "/src/vendor/autoload.php");
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../src/models"), $isDevMode, null, null, false);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/models"), $isDevMode, null, null, false);
 
-$json = file_get_contents(__DIR__ .'/config/settings.json');
+$json = file_get_contents(__DIR__ .'/src/config/settings.json');
 define('SETTINGS', json_decode($json, true));
 
 
