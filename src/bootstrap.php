6ddcc5f34655eb7ac61796fa61ae0c52777cf8ce
Cristian Steib <steibkhriz@gmail.com>
2018-09-21 15:24:55 -0300

    db credentials

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,15 +5,18 @@ use Doctrine\ORM\Tools\Setup;
 
 require_once(CODE_ROOT . "/vendor/autoload.php");
 
+$json_settings = json_decode(file_get_contents(CODE_ROOT . '/config/settings.default.json'), true);
+
+if (file_exists(CODE_ROOT . "/config/settings.json")) {
+    $json_settings_override = json_decode(file_get_contents(CODE_ROOT . '/config/settings.default.json'), true);
+    array_merge($json_settings, $json_settings_override);
+}
+
+define('SETTINGS', $json_settings, true);
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration([CODE_ROOT . "/src/models"], $isDevMode, null, null, false);
 
-$json = file_get_contents(CODE_ROOT . '/config/settings.json');
-define('SETTINGS', json_decode($json, true));
-
-
-// obtaining the entity manager
 $entityManager = EntityManager::create(SETTINGS['database'], $config);
 
