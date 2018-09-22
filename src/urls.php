c89de4fe7777ead2623816a3580c4c432cbad097
Cristian Steib <steibkhriz@gmail.com>
2018-09-22 04:19:08 -0300

    remove urls

--- src/urls.php
+++ src/urls.php
@@ -6,7 +6,6 @@ include_once(CODE_ROOT . '/core/url_dispatcher/Path.php');
 $urls = [
 
     Path::path_re("/", 'IndexController::render'),
-    Path::path_re("/index.html", 'IndexController::render'),
     Path::path_re("/login", 'LoginController::render'),
 
     Path::path_re("/auth/login", 'AuthenticationController::login'),
@@ -16,12 +15,7 @@ $urls = [
     Path::path("/modulo/usuarios/crear", 'UsuarioController::new'),
     Path::path("/modulo/pacientes", 'PacienteController::index'),
 
-
-    Path::path_re("/home/urlLinda/", 'IndexController::render'),
-    Path::path_re("/usuario/([a-zA-Z]+)/([a-zA-Z]+)/", 'IndexController::render'),
-
-
-    Path::path("/api/personas/<numero:integer>/<nombre:string>", 'IndexController::render'),
-    Path::path("/personas/<identificador:slug>/<nombre:string>", 'IndexController::render'),
+    Path::path("/api/personas/<numero:integer>/<nombre:string>", ''),
+    Path::path("/personas/<identificador:slug>/<nombre:string>", ''),
 
 ];
