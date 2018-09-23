c747edd092823d938fbf4f1be5d6b86bc537c830
Cristian Steib <steibkhriz@gmail.com>
2018-09-22 22:48:38 -0300

    add tool, to restore db easily

---
+++ src/tools/restoreDb.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cristian
+ * Date: 22/09/18
+ * Time: 22:40
+ */
+
+include_once ("../bootstrap.php");
+
+$db = SETTINGS['database']['dbname'];
+$usr = SETTINGS['database']['user'];
+$pwd = SETTINGS['database']['password'];
+$cmd = "mysql -v ". $db . " -u" . $usr . " -p" . $pwd ." < ../sql/grupo5.sql";
+echo exec($cmd);
