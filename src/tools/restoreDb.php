178694a3fb27fa8282a9977e313780918bf9f4f4
Cristian Steib <steibkhriz@gmail.com>
2018-09-22 23:10:12 -0300

    :(

--- src/tools/restoreDb.php
+++ src/tools/restoreDb.php
@@ -11,5 +11,5 @@ include_once ("../bootstrap.php");
 $db = SETTINGS['database']['dbname'];
 $usr = SETTINGS['database']['user'];
 $pwd = SETTINGS['database']['password'];
-$cmd = "mysql -v ". $db . " -u" . $usr . " -p" . $pwd ." < ../sql/grupo5.sql";
+$cmd = "mysql ". $db . " -u" . $usr . " -p" . $pwd ." < ../sql/grupo5.sql";
 echo exec($cmd);
