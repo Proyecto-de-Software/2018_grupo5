5122dfd48867337cc32b0f7d6bdb2fd54e93f611
Cristian Steib <steibkhriz@gmail.com>
2018-09-27 21:49:35 -0300

    ahora muestra todos los permisos, el index de permisos

--- src/controllers/PermisoController.php
+++ src/controllers/PermisoController.php
@@ -6,8 +6,10 @@ use controllers\Controller;
 class PermisoController extends Controller {
 
     public function index(...$args) {
-
-        echo $this->twig_render('/modules/permisos/index.html', []);
-
+        $permisos = $this->getModel('Permiso')->findAll();
+        $contexto = [
+            'permisos'=> $permisos,
+        ];
+        echo $this->twig_render('/modules/permisos/index.html', $contexto);
     }
 }
--- src/urls.php
+++ src/urls.php
@@ -43,9 +43,9 @@ function get_urls() {
         Path::path("/api/pacientes/eliminar/<id:integer>", 'PacienteController::delete'),
         Path::path("/api/pacientes/modificar/<id:integer>", 'PacienteController::update'),
 
-        Path::path("/modulo/roles", 'PermisoController->index'),
+        Path::path("/modulo/roles", 'RolController->index'),
 
-        Path::path("/modulo/permisos", 'RolController->index'),
+        Path::path("/modulo/permisos", 'PermisoController->index'),
 
 
         Path::path("/api/localidades/partido/<id:integer>", 'LocalidadController::obtenerPorPartido'),
