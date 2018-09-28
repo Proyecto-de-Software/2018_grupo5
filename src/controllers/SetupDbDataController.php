d4f2c9d01357b0a688b8a2bea951181c8a79e460
Cristian Steib <cristiansteib@gmail.com>
2018-09-28 19:01:03 -0300

    ahora tambien chequea en el codigo si se esta verificando si el sitio esta en mantenimiento

--- src/controllers/SetupDbDataController.php
+++ src/controllers/SetupDbDataController.php
@@ -21,7 +21,7 @@ class SetupDbDataController extends Controller {
         foreach ($data as $d) {
             $model = new $model();
             foreach ($d as $key => $value) {
-                if ($key == "id") {
+                if($key == "id") {
                     continue;
                 }
                 $key = ucfirst($key);
@@ -54,8 +54,8 @@ class SetupDbDataController extends Controller {
             "https://api-referencias.proyecto2018.linti.unlp.edu.ar/tipo-documento",
             "TipoDocumento"
         );
-         $time = time() - $_SERVER['REQUEST_TIME'];
-         echo "<h2> Took $time milliseconds to complete the taks. </h2>";
+        $time = time() - $_SERVER['REQUEST_TIME'];
+        echo "<h2> Took $time milliseconds to complete the taks. </h2>";
     }
 
     function generatePermissionData(...$args) {
@@ -63,7 +63,7 @@ class SetupDbDataController extends Controller {
         $controllers = (glob(CODE_ROOT . '/controllers/*Controller.php'));
         foreach ($controllers as $controller) {
             $ok = preg_match("/.+\/([A-Z][a-zA-Z]+Controller).php/", $controller, $matches);
-            if ($ok) {
+            if($ok) {
                 $class_name = $matches[1];
                 require_once($controller);
                 $reflection = new ReflectionClass($class_name);
@@ -105,11 +105,11 @@ class SetupDbDataController extends Controller {
             'sitio_activo' => 'true',
         ];
 
-        foreach ($configs as $variable=>$value){
+        foreach ($configs as $variable => $value) {
             $config = $this->getModel('Configuracion')->findOneBy(['variable' => $variable]);
             if(!isset($config)) {
                 $this->saveConfig($variable, $value);
-            } else{
+            } else {
                 echo "<p> Ya existe la config para $variable </p>";
             }
         }
@@ -134,7 +134,7 @@ class SetupDbDataController extends Controller {
         $controllers = (glob(CODE_ROOT . '/controllers/*Controller.php'));
         foreach ($controllers as $controller) {
             $ok = preg_match("/.+\/([A-Z][a-zA-Z]+Controller).php/", $controller, $matches);
-            if ($ok) {
+            if($ok) {
                 $class_name = $matches[1];
                 require_once($controller);
                 $reflection = new ReflectionClass($class_name);
@@ -148,8 +148,25 @@ class SetupDbDataController extends Controller {
                 );
                 echo "<h4>$class_name</h4>";
                 foreach ($methods as $method) {
-                    $hasAssertPermission = $this->hasAssertPermissionInFile($method->getFileName(), $method->getStartLine(), $method->getEndLine());
-                    echo '<pre>   --- ' . $method->getName() . ' --> ' . ($hasAssertPermission ? 'Analiza permisos :)' : 'Este metodo no controla los permisos!') . '</pre>';
+                    $hasAssertPermission = $this->hasContentInFile(
+                        $method->getFileName(),
+                        '->assertPermission();',
+                        $method->getStartLine(),
+                        $method->getEndLine()
+                    );
+
+                    $hasAssertInMaintenance = $this->hasContentInFile(
+                        $method->getFileName(),
+                        '->assertInMaintenance();',
+                        $method->getStartLine(),
+                        $method->getEndLine()
+
+                    );
+
+                    echo '<pre>   --- ' . $method->getName() . ' --> ';
+                    echo ($hasAssertPermission ? ' Analiza permisos ' : ' Este metodo no controla los permisos!') ;
+                    echo ($hasAssertInMaintenance ? ' Controla mantenimiento ' : ' Este metodo no controla si el sitio esta en manteniemto!') ;
+                    echo '</pre>';
                 }
             }
         }
@@ -157,14 +174,14 @@ class SetupDbDataController extends Controller {
         echo "<h2> Took $time milliseconds to complete the taks. </h2>";
     }
 
-    private function hasAssertPermissionInFile($filePath, $start, $end){
-        if ($fh = fopen($filePath, 'r')) {
-            $i=0;
+    private function hasContentInFile($filePath, $search, $start, $end) {
+        if($fh = fopen($filePath, 'r')) {
+            $i = 0;
             while (!feof($fh)) {
                 $i++;
                 $line = fgets($fh);
-                if($i >= $start && $i <= $end){
-                    if (strpos($line, '->assertPermission();') !== false) {
+                if($i >= $start && $i <= $end) {
+                    if(strpos($line, '->assertPermission();') !== false) {
                         return true;
                     }
                 }
