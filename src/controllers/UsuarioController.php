c3eadeedb0f0ea7ff95ccc5111fed6a5c718d1a4
Sebastian Muiña <sebastiangabrielm@gmail.com>
2018-09-29 19:32:32 -0300

    usuarios

--- src/controllers/UsuarioController.php
+++ src/controllers/UsuarioController.php
@@ -47,47 +47,23 @@ class UsuarioController extends Controller {
         return $instance->twig_render("modules/usuarios/user.html", $data);
     }
 
-    static function new() {
-        $instance = new UsuarioController();
-        $roles = $instance->getModel('Rol')->findAll();
-        $permissions = $instance->getModel('Permiso')->findAll();
+    function createView() {
+        $roles = $this->getModel('Rol')->findAll();
+        $permissions = $this->getModel('Permiso')->findAll();
         $context = [
             "roles" => $roles,
             "permisos" => $permissions,
         ];
-        return $instance->twig_render('modules/usuarios/crear.html', $context);
+        return $this->twig_render('modules/usuarios/formUsuario.html', $context);
     }
 
     public function create() {
         $data['error'] = false;
         $data['msg'] = 'nada';
-        $this->assertInMaintenance();
-        $this->assertPermission();
+
         $user = new Usuario();
-        $user->setFirstName($_POST['first_name']);
-        $user->setLastName($_POST['last_name']);
-        $user->setEmail($_POST['email']);
-        $user->setPassword($_POST['password']);
-        $user->setUsername($_POST['username']);
-        (!!is_null($_POST['user_state'])) ? $user->setActivo((false)) : $user->setActivo((true));
-        (!!is_null($_POST['superuser'])) ? $user->setIsSuperuser(false) : $user->setActivo(true);
-        $user->setUpdatedAt(new DateTime('now'));
+        $this->setUserData($user);
         $user->setCreatedAt(new DateTime('now'));
-
-        $roles = $_POST['roles'];
-
-        if(isset($roles)) {
-            foreach ($roles as $role) {
-                $rol = ($this->getModel('Rol')->find($role));
-                $user->addRol($rol);
-            }
-        }
-
-        foreach ($permisos as $permiso) {
-                $perm = ($this->getModel('Permiso')->find($permiso));
-                $user->addPermiso($perm);
-        }
-
         try {
             $this->entityManager()->persist($user);
             $this->entityManager()->flush();
@@ -104,14 +80,14 @@ class UsuarioController extends Controller {
     }
 
     static function delete($param) {
+
         $instance = new UsuarioController();
-        $usuarioId = $param['id'];
+        $userId = $param['id'];
         $data = [];
         if($instance->userHasPermission('usuario_destroy')) {
             try {
-                $user = $instance->getModel('Usuario')->findOneBy(['id' => $usuarioId]);
-                //$instance->entityManager()->remove($user);
-                //setear como inactivo ?
+                $user = $instance->getModel('Usuario')->findOneBy(['id' => $userId]);
+                //$user->setDelete(1);
                 $instance->entityManager()->flush();
                 $data['msg'] = "usuario eliminado con exito";
                 $data['error'] = false;
@@ -138,83 +114,95 @@ class UsuarioController extends Controller {
             "permisos" => $permissions,
             "usuario" => $user,
         ];
-        return $instance->twig_render("modules/usuarios/modificar.html", $context);
+        return $instance->twig_render("modules/usuarios/formUsuario.html", $context);
     }
 
-    static function update($param) {
-        $data = [];
-        $usuarioId = $param['id'];
+    private function setUserData($user) {
+
+        $this->assertInMaintenance();
+        $this->assertPermission();
+
+        $user->setFirstName($_POST['first_name']);
+        $user->setLastName($_POST['last_name']);
+        $user->setEmail($_POST['email']);
+        $user->setPassword($_POST['password']);
+        $user->setUsername($_POST['username']);
+        (!!is_null($_POST['user_state'])) ? $user->setActivo((false)) : $user->setActivo((true));
+        (!!is_null($_POST['superuser'])) ? $user->setIsSuperuser(false) : $user->setActivo(true);
+        $roles = $_POST['roles'];
+        if(isset($roles)) {
+            foreach ($roles as $role) {
+                $rol = ($this->getModel('Rol')->findOneBy(['id'=>$role]));
+                $user->addRol($rol);
+            }
+        }
+        $permisos = $_POST['permisos'];
+        if(isset($permisos)) {
+            foreach ($permisos as $permiso) {
+                $perm = ($this->getModel('Permiso')->find($permiso));
+                $user->addPermiso($perm);
+            }
+        }
+        $user->setUpdatedAt(new DateTime('now'));
+        return $user;
+
+
+    }
+
+    public function update() {
+        $data['error'] = false;
+        $data['msg'] = 'nada';
+        $userId = $_POST['id']; //viene por input hidden
         $instance = new UsuarioController();
-        if($instance->userHasPermission('usuario_update')) {
-            $user = $instance->getModel('Usuario')->findOneBy(['id' => $usuarioId]);
-            $user->setFirstName($_POST['first_name']);
-            $user->setLastName($_POST['last_name']);
-            $user->setEmail($_POST['email']);
-            $user->setUsername($_POST['username']);
-            $user->setActivo($_POST['user_state']);
-            $user->setUpdatedAt(new DateTime('now'));
-            $roles = $_POST['roles'];
-            if(isset($roles)) {
-                foreach ($roles as $role) {
-                    $rol = ($instance->getModel('Rol')->find($role));
-                    $user->addRol($rol);
-                }
-            }
-            $permissions = $_POST['permisos'];
-            if(isset($permissions)) {
-                foreach ($permissions as $permission) {
-                    $perm = ($instance->getModel('Permiso')->find($permission));
-                    $user->addPermiso($perm);
-                }
-            }
+        $user = $instance->getModel('Usuario')->findOneBy(['id' => $userId]);
+        try {
+            $instance->entityManager()->merge($instance->setUserData($user));
+            $instance->entityManager()->flush();
+            $data['msg'] = "usuario actualizado ok";
+        } catch (Exception $e) {
+            $data = [
+                "msg" => "Error al actualizar los datos del usuario" . $e->getMessage(),
+                "error" => true,
+            ];
+        }
+        return $this->jsonResponse($data);
+    }
+
+
+
+static function changePassword_view($param) {
+    $instance = new UsuarioController();
+    $user = $instance->getModel('Usuario')->findOneBy(['id' => $param['id']]);
+    $context['usuario'] = $user;
+    return $instance->twig_render("modules/usuarios/cambiarClave.html", $context);
+}
+
+static function changePassword($param) {
+    $data = [];
+    $old_password = $_POST['old_pwd'];
+    $new_password = $_POST['new_pwd'];
+    $usuarioId = $param['id'];
+    $instance = new UsuarioController();
+    if($instance->userHasPermission('usuario_update')) {
+        $user = $instance->getModel('Usuario')->findOneBy(['id' => $usuarioId]);
+        if($user->getPassword() == $old_password) {
+            $user->setPassword($new_password);
             try {
                 $instance->entityManager()->persist($user);
                 $instance->entityManager()->flush();
-                $data['msg'] = "usuario actualizado ok";
+                $data['msg'] = "clave actualizada ok";
             } catch (Exception $e) {
                 $data['msg'] = $e->getMessage();
             }
-
         } else {
-            $data['error'] = true;
-            $data['msg'] = "Not enough permission or not logged";
+            $data['msg'] = "The old password didn't match";
         }
-        (DEBUG) ? var_dump("msg:" . $data['msg']) : header('Location: /modulo/usuarios');
+    } else {
+        $data['error'] = true;
+        $data['msg'] = "Not enough permission or not logged";
     }
-
-    static function changePassword_view($param) {
-        $instance = new UsuarioController();
-        $user = $instance->getModel('Usuario')->findOneBy(['id' => $param['id']]);
-        $context['usuario'] = $user;
-        return $instance->twig_render("modules/usuarios/cambiarClave.html", $context);
-    }
-
-    static function changePassword($param) {
-        $data = [];
-        $old_password = $_POST['old_pwd'];
-        $new_password = $_POST['new_pwd'];
-        $usuarioId = $param['id'];
-        $instance = new UsuarioController();
-        if($instance->userHasPermission('usuario_update')) {
-            $user = $instance->getModel('Usuario')->findOneBy(['id' => $usuarioId]);
-            if($user->getPassword() == $old_password) {
-                $user->setPassword($new_password);
-                try {
-                    $instance->entityManager()->persist($user);
-                    $instance->entityManager()->flush();
-                    $data['msg'] = "clave actualizada ok";
-                } catch (Exception $e) {
-                    $data['msg'] = $e->getMessage();
-                }
-            } else {
-                $data['msg'] = "The old password didn't match";
-            }
-        } else {
-            $data['error'] = true;
-            $data['msg'] = "Not enough permission or not logged";
-        }
-        (DEBUG) ? var_dump("msg:" . $data['msg']) : header('Location: /modulo/usuarios');
-    }
+    (DEBUG) ? var_dump("msg:" . $data['msg']) : header('Location: /modulo/usuarios');
+}
 
 
 }
--- src/urls.php
+++ src/urls.php
@@ -16,13 +16,15 @@ function get_urls() {
 
         Path::path("/modulo/usuarios", 'UsuarioController::index'),
         Path::path("/modulo/usuarios/<id:integer>", 'UsuarioController::ver'),
-        Path::path("/modulo/usuarios/crear", 'UsuarioController::new'),
+        Path::path("/modulo/usuarios/crear", 'UsuarioController->createView'),
         Path::path("/modulo/usuarios/modificar/<id:integer>", 'UsuarioController::update_view'),
         Path::path("/modulo/usuarios/buscar", 'UsuarioController::searchView'),
         Path::path("/modulo/usuarios/busqueda", 'UsuarioController::search'),
         Path::path("/api/usuarios/eliminar/<id:integer>", 'UsuarioController::delete'),
+
         Path::path("/api/usuarios/crear", 'UsuarioController->create'),
-        Path::path("/api/usuarios/modificar/<id:integer>", 'UsuarioController::update'),
+        Path::path("/api/usuarios/modificar", 'UsuarioController->update'),
+
         Path::path("/modulo/usuarios/<id:integer>/cambiarClave", 'UsuarioController::changePassword_view'),
         Path::path("/api/usuarios/<id:integer>/cambiarClave", 'UsuarioController::changePassword'),
 
