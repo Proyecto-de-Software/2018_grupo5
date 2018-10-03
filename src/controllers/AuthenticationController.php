8501be98e07f9a6aaa8aced5083e84e2da2db010
Cristian Steib <cristian.steib@avature.net>
2018-10-03 16:40:34 -0300

    login redirects

--- src/controllers/AuthenticationController.php
+++ src/controllers/AuthenticationController.php
@@ -1,42 +1,47 @@
 <?php
 require_once(CODE_ROOT . "/controllers/Controller.php");
+
 use controllers\Controller;
 
 class AuthenticationController extends Controller {
 
-    public function login(){
+    public function login() {
         $usr = $_POST['username'];
         $psw = $_POST['password'];
         $user = $this->getModel('Usuario')->findOneBy(
-            array(
-                'email'=> $usr,
-                'password'=> $psw,
-                'activo'=>true
-            )
+            [
+                'email' => $usr,
+                'password' => $psw,
+                'activo' => true,
+            ]
         );
 
         // if the user didn't founded, search with the username now
-        if (!isset($user)){
+        if(!isset($user)) {
             $user = $this->getModel('Usuario')->findOneBy(
-                array(
-                    'username'=> $usr,
-                    'password'=> $psw,
-                    'activo'=>true
-                )
+                [
+                    'username' => $usr,
+                    'password' => $psw,
+                    'activo' => true,
+                ]
             );
         }
 
-        if (isset($user)) {
-            $this->session->createAuthenticatedSession($user->getId(),[]);
+        if(isset($user)) {
+            $this->session->createAuthenticatedSession($user->getId(), []);
+            if($user->getIsSuperuser()) {
+                $this->redirect('/modulo/configuracion');
+            }
             $this->redirect('/');
+
         } else {
-             return $this->twig_render('/login.html',['error'=>true]);
+            return $this->twig_render('/login.html', ['error' => true]);
         }
         error_log("algo raro paso en el login");
     }
 
-   public function logout(){
+    public function logout() {
         $this->session->destroyAuthenticatedSession();
         $this->redirect('/');
-   }
+    }
 }
