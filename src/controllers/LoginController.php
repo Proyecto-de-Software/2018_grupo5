0581bec0144ecdca313fce4b554cd79dbb81776c
Cristian Steib <cristiansteib@gmail.com>
2018-10-17 00:52:37 -0300

    remove unused code

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -5,18 +5,8 @@ use controllers\Controller;
 
 class LoginController extends Controller {
 
-    public static function render(...$args) {
-        //echo "Hola mundo! esto es el index ";
-        //var_dump($args);
-
+    public static function render() {
         $instance = new LoginController();
-
-        $parameters = [
-            'titulo' => 'Login',
-            'apellido' => 'Gonzalez',
-            'id' => ($args[0]['identificador'] ?? 'None'),
-        ];
-        echo $instance->twig_render('login.html', $parameters);
-
+        echo $instance->twig_render('login.html', []);
     }
 }
