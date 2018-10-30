577da1e665a2075eb5693aa784f142cc86c08163
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-10-29 22:46:15 -0300

    formulario de alta de consulta

--- src/controllers/consultaController.php
+++ src/controllers/consultaController.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 require_once(CODE_ROOT . "/controllers/Controller.php");
 require_once(CODE_ROOT . "/models/Paciente.php");
 require_once(CODE_ROOT . "/models/Genero.php");
@@ -9,7 +9,7 @@ require_once(CODE_ROOT . "/models/TipoDocumento.php");
 require_once(CODE_ROOT . "/models/Localidad.php");
 require_once(CODE_ROOT . "/models/Partido.php");
 require_once(CODE_ROOT . "/models/ObraSocial.php");
-*/
+
 use controllers\Controller;
 
 class ConsultaController extends Controller {
