b1c9a073b775195cf5a8af93f445699169d2c4ca
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-10-29 23:10:32 -0300

    formulario de alta de consulta

--- src/controllers/ConsultaController.php
+++ src/controllers/ConsultaController.php
@@ -1,22 +1,28 @@
 <?php
 //
 require_once(CODE_ROOT . "/controllers/Controller.php");
-require_once(CODE_ROOT . "/models/Paciente.php");
-require_once(CODE_ROOT . "/models/Genero.php");
-require_once(CODE_ROOT . "/models/RegionSanitaria.php");
-require_once(CODE_ROOT . "/models/TipoDocumento.php");
-require_once(CODE_ROOT . "/models/Localidad.php");
-require_once(CODE_ROOT . "/models/Partido.php");
-require_once(CODE_ROOT . "/models/ObraSocial.php");
+require_once(CODE_ROOT . "/models/MotivoConsulta.php");
+require_once(CODE_ROOT . "/models/Acompanamiento.php");
+require_once(CODE_ROOT . "/models/TratamientoFarmacologico.php");
 
 use controllers\Controller;
 
 class ConsultaController extends Controller {
 
     public function createView(){
-        
-        return $this->twig_render("modules/consultas/formConsulta.html", []);
+        $motivos = $this->getModel('MotivoConsulta')->findAll();
+        $acompanamientos = $this->getModel('Acompanamiento')->findAll();
+        $tratamientos_farmacologicos = $this->getModel('TratamientoFarmacologico')->findAll();
+        $parameters = [
+            'motivos' => $motivos,
+            'acompanamientos' => $acompanamientos,
+            'tratamientos_farmacologicos' => $tratamientos_farmacologicos,
+        ];
+
+        return $this->twig_render("modules/consultas/formConsulta.html", $parameters);
 
     }
 }
+
+
 
