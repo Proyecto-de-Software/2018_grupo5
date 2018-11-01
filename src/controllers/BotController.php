6c5fdecc984a16c4f4fd21c923da2b2434527519
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-11-01 20:49:15 -0300

    probando bot telegram

--- src/controllers/BotController.php
+++ src/controllers/BotController.php
@@ -58,10 +58,10 @@ class BotController {
             $data = json_decode(file_get_contents('https://grupo5.proyecto2018.linti.unlp.edu.ar/api/instituciones/'));
 
 
-            $msg['text']  = 'Las instituciones disponible son' . PHP_EOL;
-            foreach ($data as $institucion) {
+            $msg['text']  = 'Las instituciones disponible son' . var_dump($data);
+        /*   foreach ($data as $institucion) {
                 $msg['text'] .= $institucion['nombre']. ", Calle ".$institucion['direccion']. PHP_EOL;
-            }
+            }*/
             $msg['reply_to_message_id'] = null;
             break;
 
