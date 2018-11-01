7d9c94f1fb589e6fd092c8c337c38a2a2908be4c
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-11-01 18:55:19 -0300

    probando bot telegram

--- src/urls.php
+++ src/urls.php
@@ -41,6 +41,8 @@ function get_urls() {
         Path::path("/api/pacientes/crear", 'PacienteController->create'),
         Path::path("/api/pacientes/crear-nn", 'PacienteController->createNN'),
 
+        Path::path("/bot", 'BotController->index'),
+
         Path::path("/api/pacientes", 'PacienteController->pacientesJSON'),
 
         Path::path("/api/pacientes/eliminar/<id:integer>", 'PacienteController::delete'),
