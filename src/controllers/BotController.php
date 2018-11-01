de6b05479a969bb40aac299af5523857cc4db24e
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-11-01 20:39:38 -0300

    probando bot telegram

--- src/controllers/BotController.php
+++ src/controllers/BotController.php
@@ -58,7 +58,7 @@ class BotController {
             $data = json_decode(file_get_contents('/api/instituciones/'));
 
 
-            $msg['text']  = 'Las instituciones disponible son' . echo ($data[0]['nombre']);
+            $msg['text']  = 'Las instituciones disponible son' . var_dump($data);
            /* foreach ($data as $institucion) {
                 $msg['text'] .= $institucion['nombre']. ", Calle ".$institucion['direccion']. PHP_EOL;
             }*/
