424515ae2ea85b8f14263c8ed530fed6c90489c7
Leonardo Nicolas Armendariz <leoarmendariz@fibertel.com.ar>
2018-11-01 20:07:30 -0300

    probando bot telegram

--- src/controllers/BotController.php
+++ src/controllers/BotController.php
@@ -73,8 +73,8 @@ class BotController {
                 break;
         }
 
-
-        $url = 'https://api.telegram.org/bot798730946:AAHtnDjJnj63AbDK6qEKag9GE61FRjLHIMM/sendMessage';
+$test=var_dump($response);
+        $url = 'https://api.telegram.org/bot798730946:AAHtnDjJnj63AbDK6qEKag9GE61FRjLHIMM/sendMessage?text=$test&&chat_id=591767949';
 
         $options = array(
         'http' => array(
