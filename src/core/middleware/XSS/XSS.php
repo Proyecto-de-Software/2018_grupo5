dfb100d0adb7bb819852f20e6955471475787320
Cristian Steib <cristiansteib@gmail.com>
2018-11-05 16:07:30 -0300

    XSS Fixes

--- src/core/middleware/XSS/XSS.php
+++ src/core/middleware/XSS/XSS.php
@@ -4,7 +4,7 @@
  * Date: 29/10/18
  * Time: 23:31
  */
-/*
+
 function html_scape(&$value) {
     $value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
@@ -16,4 +16,3 @@ function filterArray($array) {
 
 $_POST = filterArray($_POST);
 $_GET = filterArray($_GET);
-*/
