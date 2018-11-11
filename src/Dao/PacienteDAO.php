2eea257a0e14d08b7b348f4bf9d2480bfda34b92
Sebastian Muiña <sebastiangabrielm@gmail.com>
2018-11-11 14:35:01 -0300

    paciente update y delete ya no son estaticos. DAO a paciente. espero que no explote

--- src/Dao/PacienteDAO.php
+++ src/Dao/PacienteDAO.php
@@ -13,4 +13,40 @@ class PacienteDAO extends DAO {
 
     public $model = "Paciente";
 
+    public function searchPacientes($nombre, $apellido, $tipo_doc, $doc_numero, $numeroHistorioClinica, $deleted){
+
+        $qb = $this->entityManager()->createQueryBuilder();
+        $qb->select('p')
+            ->from('Paciente', 'p')
+            ->where($qb->expr()->orX(
+                $qb->expr()->like('p.nombre', '?1'),
+                $qb->expr()->like('p.apellido', '?2'),
+                $qb->expr()->andX(
+                    $qb->expr()->eq('p.tipoDoc', '?3'),
+                    $qb->expr()->eq('p.numero', '?4')
+                ),
+                $qb->expr()->eq('p.nroHistoriaClinica', '?5')
+            ),
+                $qb->expr()->andX(
+                    $qb->expr()->eq('p.eliminado', '?6')
+                )
+            );
+        $parameters =
+            [
+                1 => '',
+                2 => '',
+                3 => $tipo_doc,
+                4 => $doc_numero,
+                5 => $numeroHistorioClinica,
+                6 => $deleted,
+            ];
+
+        $parameters[1] = ($nombre !== "") ? "%" . $nombre . "%" : $nombre;
+        $parameters[2] = ($apellido !== "") ? "%" . $apellido . "%" : $apellido;
+
+        $qb->setParameters($parameters);
+        $query = $qb->getQuery();
+        return $query->getResult();
+    }
+
 }
