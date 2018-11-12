58150f6c8b38226758ec85f30c69413b48fc86f6
ARMENDARIZ, LEONARDO NICOLAS <leoarmendariz@fibertel.com.ar>
2018-11-12 18:36:01 -0300

    Update ConsultaDAO.php

--- src/Dao/ConsultaDAO.php
+++ src/Dao/ConsultaDAO.php
@@ -5,7 +5,7 @@ class ConsultaDAO extends DAO {
     public $model = "Consulta";
 
     public function getConsultasByPaciente($paciente){
-        if ($paciente !=null && $paciente.getEliminado() == '0') {
+        if ($paciente !=null && $paciente->getEliminado() == '0') {
             return $this->findBy(['paciente' => $paciente, 'eliminado'=>'0']);
         }
         return [];
