be6a440b7969e2eb13643f70c5c31dac067e21af
Cristian Steib <cristian.steib@avature.net>
2018-11-12 15:47:53 -0300

    add partidoController

---
+++ src/controllers/PartidoController.php
@@ -0,0 +1,23 @@
+<?php
+require_once(CODE_ROOT . "/controllers/Controller.php");
+
+use controllers\Controller;
+
+class PartidoController extends Controller {
+
+    private $partidoDao;
+
+    function __construct() {
+        parent::__construct();
+        $this->partidoDao = new PartidoDao();
+    }
+
+    function verTodosLosPartidos() {
+        $data = [];
+        $partidos = $this->partidoDao->getAll();
+        foreach ($partidos as $partido) {
+            $data[] = ['id' => $partido->getId()];
+        }
+        $this->jsonResponse($data);
+    }
+}
