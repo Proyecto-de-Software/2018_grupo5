f195cdaceed3f7be4f99c51b5d7e19dcdb844ccc
Cristian Steib <cristiansteib@gmail.com>
2018-11-16 12:33:23 -0300

    define a constant for 'sitio_activo' instead of repeating this literal

--- src/controllers/ConfiguracionController.php
+++ src/controllers/ConfiguracionController.php
@@ -5,6 +5,8 @@ use controllers\Controller;
 
 class ConfiguracionController extends Controller {
     private $confiDao;
+    const sitio_activo = "sitio_activo";
+
 
     function __construct() {
         parent::__construct();
@@ -14,7 +16,7 @@ class ConfiguracionController extends Controller {
     function indexView(...$args) {
         $this->assertPermission();
         $parameters = [
-            'sitio_activo' => $this->confiDao->getConfigValue('sitio_activo'),
+            self::sitio_activo => $this->confiDao->getConfigValue(self::sitio_activo),
         ];
         return $this->twig_render("modules/configuracion/index.html", $parameters);
     }
@@ -46,7 +48,7 @@ class ConfiguracionController extends Controller {
 
     function setMantenimiento(...$args) {
         $this->assertPermission();
-        $this->setSetting('sitio_activo', (string)$_POST['sitio_activo']);
+        $this->setSetting(self::sitio_activo, (string)$_POST[self::sitio_activo]);
         return $this->jsonResponse([
             'ok' => true,
         ]);
