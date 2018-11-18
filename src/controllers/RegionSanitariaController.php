e0a608925cd999d5d4c54d67dc87812ccfdc287f
Sebastian Muiña <sebastiangabrielm@gmail.com>
2018-11-18 13:25:15 -0300

    reportes en la misma pantalla. refactor localidad y region sanitaria

--- src/controllers/RegionSanitariaController.php
+++ src/controllers/RegionSanitariaController.php
@@ -5,21 +5,26 @@ use controllers\Controller;
 
 class RegionSanitariaController extends Controller {
 
-    
-    static function obtenerPorPartido($id_partido){
-        /** @todo FIX THIS */
-        $instance = new RegionSanitariaController();
-        $instance->assertPermission();
-        $partidoDao = new PartidoDao();
-        $partido_a_buscar = $partidoDao->getById($id_partido[1]);
-        $region_encontrada= $partido_a_buscar->getRegionSanitaria()->getNombre();
-        $id_region_encontrada = $partido_a_buscar->getRegionSanitaria()->getId();
-        $arrayRegion = array('id_region_sanitaria' => $id_region_encontrada, 'nombre' => $region_encontrada);
-        return json_encode($arrayRegion);
+    private $regionSAnitariaDao;
+    private $partidoDao;
+
+    function __construct() {
+        parent::__construct();
+        $this->partidoDao = new PartidoDao();
+        $this->regionSAnitariaDao = new RegionSanitariaDAO();
     }
 
+    function obtenerPorPartido($id_partido) {
 
-
-
+        $this->assertPermission();
+        $partido_a_buscar = $this->partidoDao->getById($id_partido[1]);
+        if($partido_a_buscar == null) {
+            return $this->jsonResponse(['error'=>true, 'msg'=>'Not Found']);
+        }
+        $region_encontrada = $partido_a_buscar->getRegionSanitaria()->getNombre();
+        $id_region_encontrada = $partido_a_buscar->getRegionSanitaria()->getId();
+        $arrayRegion = ['id_region_sanitaria' => $id_region_encontrada, 'nombre' => $region_encontrada];
+        return $this->jsonResponse($arrayRegion);
+    }
 
 }
--- src/urls.php
+++ src/urls.php
@@ -74,7 +74,7 @@ function get_urls() {
 
 
         Path::path("/api/localidades/partido/<id:integer>", 'LocalidadController->obtenerPorPartido'),
-        Path::path("/api/region_sanitaria/partido/<id:integer>", 'RegionSanitariaController::obtenerPorPartido'),
+        Path::path("/api/region_sanitaria/partido/<id:integer>", 'RegionSanitariaController->obtenerPorPartido'),
         Path::path("/api/partidos", 'PartidoController->verTodosLosPartidos'),
 
 
