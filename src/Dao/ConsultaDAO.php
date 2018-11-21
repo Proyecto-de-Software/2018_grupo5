8b6bfa147da44ac00444618611cf400fcf7911f4
Cristian Steib <cristian.steib@avature.net>
2018-11-21 14:09:52 -0300

    SQ code smells

--- src/Dao/ConsultaDAO.php
+++ src/Dao/ConsultaDAO.php
@@ -16,7 +16,7 @@ class ConsultaDAO extends DAO {
 
         $qb = $this->entityManager()->createQueryBuilder();
         $qb->select('mc.nombre as name', 'count(c.motivo) as y')
-            ->from('Consulta', 'c')
+            ->from($this->model, 'c')
             ->join('MotivoConsulta', 'mc', 'WITH', 'c.motivo = mc.id')
             ->groupBy("c.motivo");
         $query = $qb->getQuery();
@@ -26,7 +26,7 @@ class ConsultaDAO extends DAO {
     public function getGroupedByGender() {
         $qb = $this->entityManager()->createQueryBuilder();
         $qb->select('g.nombre as name', 'count(g.nombre) as y')
-            ->from('Consulta', 'c')
+            ->from($this->model, 'c')
             ->join('Paciente', 'p', 'WITH', 'c.paciente = p.id')
             ->join('Genero', 'g', 'WITH', 'g.id = p.genero')
             ->groupBy("g.nombre");
@@ -37,7 +37,7 @@ class ConsultaDAO extends DAO {
     public function getGroupedByLocation() {
         $qb = $this->entityManager()->createQueryBuilder();
         $qb->select('l.nombre as name', 'count(l.nombre) as y')
-            ->from('Consulta', 'c')
+            ->from($this->model, 'c')
             ->join('Paciente', 'p', 'WITH', 'c.paciente = p.id')
             ->join('Localidad', 'l', 'WITH', 'l.id = p.localidad')
             ->groupBy("l.nombre");
